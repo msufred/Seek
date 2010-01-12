bf82e4554a7135806169c6af0deee2411f87fe90
msufred <gemseeker24@gmail.com>
2010-01-13 06:28:59 +0800

    modified, added startActivityFromParent functions, added javadocs

--- src/gem/seek/Context.kt
+++ src/gem/seek/Context.kt
@@ -8,6 +8,24 @@ package gem.seek
  * Created by Gem Seeker on 4/19/2017.
  */
 interface Context {
+    /**
+     * Starts new Activity. This method do all the instantiating, creating and starting an Activity.
+     */
     fun startActivity(activity: Class<out Activity>)
+
+    /**
+     * Starts new Activity. This method assumes that Activity has been created.
+     */
     fun startActivity(activity: Activity)
+
+    /**
+     * Starts new Activity from parent Activity. It is the same as [startActivity] but with additional task, setting
+     * next Activity's parent Activity. Calling [Activity.onBackPressed] will start Activity's parent Activity.
+     */
+    fun startActivityFromParent(activity: Class<out Activity>, parentActivity:Activity)
+
+    /**
+     * Starts new Activity from parent Activity. This method assumes that the Activity has been created.
+     */
+    fun startActivityFromParent(activity: Activity, parentActivity: Activity)
 }
