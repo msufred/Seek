fa4b31ee63acf2925a309d2ee8aa25a54f132f26
msufred <gemseeker24@gmail.com>
2010-01-13 06:26:19 +0800

    added global variables

--- src/gem/seek/util/GlobalConfig.kt
+++ src/gem/seek/util/GlobalConfig.kt
@@ -2,3 +2,8 @@ package gem.seek.util
 
 const val DEBUG = true
 const val LOG_TO_FILE = false
+
+// Activity Global Configuration
+const val ACTIONBAR_SIZE = 56.0
+const val TOAST_SIZE = 56.0
+const val TOAST_ANIM_DURATION = 400.0
