927128638a3fd3ac152b019517d1340d743f605d
msufred <gemseeker24@gmail.com>
2010-01-13 06:31:14 +0800

    added javadoc

--- src/gem/seek/ApplicationContext.kt
+++ src/gem/seek/ApplicationContext.kt
@@ -6,5 +6,12 @@ package gem.seek
  * Created by Gem Seeker on 7/18/2018.
  */
 abstract class ApplicationContext : Context {
+
+    /**
+     * Creates and returns [FragmentManager] instance for the [Activity]. If a [FragmentManager] instance was already created,
+     * it will return it instead.
+     *
+     * @return [FragmentManager] instance for the [Activity]
+     */
     abstract fun getFragmentManager(activity: Activity): FragmentManager?
 }
